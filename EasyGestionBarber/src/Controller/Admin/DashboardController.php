6cf5a7eb1e680207a2e9602e6efd3e36ce540d49
hichemchems <hichem.djacta@gmail.com>
2025-11-04 01:31:51 +0100

    Add Route attribute to DashboardController index method to fix EasyAdmin routing

--- EasyGestionBarber/src/Controller/Admin/DashboardController.php
+++ EasyGestionBarber/src/Controller/Admin/DashboardController.php
@@ -7,10 +7,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         return parent::index();
