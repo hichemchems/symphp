c3121a3c41a88f391aacabdf137dd67769d99e71
hichemchems <132211343+hichemchems@users.noreply.github.com>
2025-11-05 15:31:44 +0100

    "Added ORM\Table annotation to WeeklyCommission entity."

--- src/Entity/WeeklyCommission.php
+++ src/Entity/WeeklyCommission.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: WeeklyCommissionRepository::class)]
+#[ORM\Table(name: "weekly_commissions")]
 class WeeklyCommission
 {
     #[ORM\Id]
